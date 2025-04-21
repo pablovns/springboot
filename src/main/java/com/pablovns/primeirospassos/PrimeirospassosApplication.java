0025c82ea7e59a65737581651f4bf1308dd39fcc
pablovns <viniciuspablo962@gmail.com>
2025-04-21 18:05:38 -0300

    initial commit

---
+++ src/main/java/com/pablovns/primeirospassos/PrimeirospassosApplication.java
@@ -0,0 +1,13 @@
+package com.pablovns.primeirospassos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeirospassosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrimeirospassosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pablovns/primeirospassos/PrimeirospassosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pablovns.primeirospassos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeirospassosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
