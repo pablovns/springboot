c829239861d93ffc5fd22acbcde5bf6f685408c9
pablovns <viniciuspablo962@gmail.com>
2025-04-24 22:01:17 -0300

    refactor: remover código não utilizado

--- src/main/java/com/pablovns/primeirospassos/StartApp.java
+++ src/main/java/com/pablovns/primeirospassos/StartApp.java
@@ -1,17 +1,14 @@
 package com.pablovns.primeirospassos;
 
-import com.pablovns.primeirospassos.model.Usuario;
 import com.pablovns.primeirospassos.repository.UsuarioRepository;
-import com.pablovns.primeirospassos.util.EmailGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
-
 @Component
 public class StartApp implements CommandLineRunner {
 
+    // Maneira apropriada de fazer uma injeção de dependência (injeta no construtor ao invés de injetar na variável)
     private final UsuarioRepository userRepository;
 
     @Autowired
@@ -21,20 +18,6 @@ public class StartApp implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        for (int i = 0; i < 5; i++) {
-            Usuario usuario = new Usuario();
-
-            final String nomeGerado = EmailGenerator.generateRandomUsername();
-            usuario.setNome(nomeGerado);
-            usuario.setEmail(EmailGenerator.generateRandomEmail(nomeGerado));
-            usuario.setSenha(nomeGerado + "12345");
-            usuario.setCreatedAt(LocalDateTime.now());
-            Thread.sleep(100);
-//            userRepository.save(usuario);
-        }
-
-        for (Usuario u : userRepository.findAll()) {
-            System.out.println(u);
-        }
+        System.out.println("Iniciando app...");
     }
 }
